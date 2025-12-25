f09ac96fd3e1e07d75047c97ac68a31f5fbeb33c
RushabRisal <rushabofficials@gmail.com>
2025-12-25 23:45:31 +0545

    learn/interface

---
+++ InterfaceAndAbstract/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace InterfaceAndAbstract;
+
+interface ISample
+{
+    void DisplayHello();
+}
+public class Test : ISample
+{
+
+    void ISample.DisplayHello()
+    {
+        Console.WriteLine("hello world");
+    }
+}
+public class Dog: ISample
+{
+    void ISample.DisplayHello()
+    {
+        Console.WriteLine("Hello I am dog");
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        ISample world = new Test();
+        ISample dog = new Dog();
+        world.DisplayHello();
+        dog.DisplayHello();
+    }
+}
